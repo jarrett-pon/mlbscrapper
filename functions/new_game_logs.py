58ed0010938d19f0d99d5b322687edae028783b5
Jarrett Pon <jarrett.pon@gmail.com>
2018-03-31 18:52:24 -0700

    Add try catch for new game logs

--- functions/new_game_logs.py
+++ functions/new_game_logs.py
@@ -13,13 +13,23 @@ def new_game_logs(updated_game_logs, old_game_logs, game_id_index):
     updated_count = len(updated_game_logs)
 
     while updated_index < updated_count:
-        old_players_id = old_game_logs[old_index][0]
+        try:
+            old_players_id = old_game_logs[old_index][0]
+        except IndexError:
+            print("Error: Old index exceeded range in old game logs")
+            print("Old index: {}".format(old_index))
+            print("Old count: {}".format(old_count))
+            print("Updated index: {}".format(updated_index))
+            print("Updated count: {}".format(updated_count))
+            updated_index += 1
+            continue
+
         updated_players_id = updated_game_logs[updated_index][0]
         old_game_id = old_game_logs[old_index][game_id_index]
         updated_game_id = updated_game_logs[updated_index][game_id_index]
 
-        if (old_index < old_count and old_players_id == updated_players_id and
-                old_game_id == updated_game_id):
+        if (old_players_id == updated_players_id and
+                old_game_id == updated_game_id and old_index < old_count):
             old_index += 1
         else:
             new_game_logs.append(updated_game_logs[updated_index])
